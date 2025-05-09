fa9aee01e6d780699a7a89bb491926926241076c
Cas <76619307+Cassa-sys@users.noreply.github.com>
2025-05-08 23:05:02 -0400

    Fix image pathing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,12 @@ const carouselConfig = {
   wrapAround: true,
 }
 
-const images = ['src/assets/images/image1.png', 'src/assets/images/image2.png', 'src/assets/images/image3.png']
+import image1 from '@/assets/images/image1.png';
+import image2 from '@/assets/images/image2.png';
+import image3 from '@/assets/images/image3.png';
+
+const images = [image1, image2, image3]
+
 </script>
 
 <template>
